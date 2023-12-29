530f0b1375b5c0cba8e052cedf16a538bc538476
Samyog Koirala <126430228+samyog1234@users.noreply.github.com>
2023-12-29 21:57:36 +0545

    tooltip and sized correction in app bar

--- Simple_App_Bar.dart
+++ Simple_App_Bar.dart
@@ -16,34 +16,54 @@ class Fyou extends StatelessWidget{
       title: const Text("Hello",
                        style: TextStyle(
                        color: Colors.blue,
-                       fontSize: 40)),
-        flexibleSpace: Center(
-          child: SizedBox(
-            width: 200,
-            height: 500,
-          child: IconButton(
-            iconSize: 60,
-            icon: const Icon(Icons.favorite),
-            onPressed: () {},
-            color: Colors.blue,
-            ),
+                       fontSize: 30)),
+        flexibleSpace: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 80,
+                height: 90,
+                child: IconButton(
+                  iconSize: 40,
+                  icon: const Icon(Icons.favorite),
+                  onPressed: () {
+                    print("Love Reacted");
+                  },
+                  color: Colors.pink,
+                  tooltip: "Love",
+                ),
+              ),
+              const SizedBox(width: 8),
+              const Text(
+                'react',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
-        ), 
         actions: [
           IconButton(
           icon: const Icon(Icons.search),
-            onPressed: () {},
+            onPressed: () {
+              print("Searched Clicked");
+            },
             color: Colors.red,
+            tooltip: "Search",
           ),
         ],
         leading: IconButton(
         icon: const Icon(Icons.menu),
-          onPressed: () {},
+          onPressed: () {
+            print("Menu clicked");
+          },
           color: Colors.blue,
+          tooltip: "Menu",
         ),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.black87,
       ),
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.brown,
       )
     );
   }
